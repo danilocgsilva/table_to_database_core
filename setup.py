b7d1cb2514c0b85e500c29ae60c75be1faad7655
Danilo Silva <danilo@danilocgsilva.me>
2026-03-24 08:32:11 -0300

    Updated version
    setup.py have a mismatching version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='table_to_database',
-    version='1.1',
+    version='1.2.1',
     packages=find_packages(),
     install_requires=[
         'pyexcel_ods',
